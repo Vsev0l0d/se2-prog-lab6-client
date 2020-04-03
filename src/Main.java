feedd90f7a46d3c820f784a0500173fbde2c4294
Aurelius <mraurelius@yandex.ru>
2020-04-04 01:32:21 +0300

    branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
 public class Main {
-    public static void main(String[] args){
-        Client client = new Client();
-        client.run();
+    public static void main(String[] args) {
+        try {
+            InetSocketAddress address = new InetSocketAddress("localhost", 4321);
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(address);
+            System.out.println(socketChannel.finishConnect());
+            for (int i = 0; i < 2; i++) {
+                ByteBuffer writeBuffer = ByteBuffer.allocate(8192);
+                writeBuffer.clear();
+                writeBuffer.put("heartbeat".getBytes());
+
+                writeBuffer.flip();
+                while(writeBuffer.hasRemaining()) {
+                    socketChannel.write(writeBuffer);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
