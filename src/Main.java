1862fc2b1c925adfe01d5036d96007a62d1ac8d4
Aurelius <mraurelius@yandex.ru>
2020-04-05 18:16:03 +0300

    branch

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,49 @@
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+
+class Zalupa implements Serializable {
+    public int size;
+
+    public Zalupa(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private void readObject(ObjectInputStream test)throws
+            IOException, ClassNotFoundException
+    {
+        // Use of defaultReadObject() :
+        test.defaultReadObject();
+    }
+}
+
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            InetSocketAddress address = new InetSocketAddress("localhost", 4321);
-            SocketChannel socketChannel = SocketChannel.open();
-            socketChannel.configureBlocking(false);
-            socketChannel.connect(address);
-            System.out.println(socketChannel.finishConnect());
-            for (int i = 0; i < 2; i++) {
-                ByteBuffer writeBuffer = ByteBuffer.allocate(8192);
-                writeBuffer.clear();
-                writeBuffer.put("heartbeat".getBytes());
+    public static void main(String[] args) throws IOException, InterruptedException {
+        InetSocketAddress crunchifyAddr = new InetSocketAddress("localhost", 4321);
+        SocketChannel crunchifyClient = SocketChannel.open(crunchifyAddr);
 
-                writeBuffer.flip();
-                while(writeBuffer.hasRemaining()) {
-                    socketChannel.write(writeBuffer);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        log("Connecting to Server on port 4321...");
+
+        Zalupa zalupa = new Zalupa(5);
+        System.out.println(zalupa.getSize());
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(zalupa);
+        byte [] data = bos.toByteArray();
+        crunchifyClient.write(ByteBuffer.wrap(data)); //write
+
+        crunchifyClient.close();
     }
+
+    private static void log(String str) {
+        System.out.println(str);
+    }
+
 }
