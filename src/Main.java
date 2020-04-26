528e7ddb1e91ab166b0f14920b723500eb64a065
avelok <mraurelius@yandex.ru>
2020-04-26 16:05:18 +0300

    Merge branch 'testing' of https://github.com/Vsev0l0d/se2-prog-lab6-client into testing
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,8 @@
 import java.io.*;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
-import java.util.ArrayList;
-
-class Zalupa implements Serializable {
-    public int size;
-
-    public Zalupa(int size) {
-        this.size = size;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    private void readObject(ObjectInputStream test)throws
-            IOException, ClassNotFoundException
-    {
-        // Use of defaultReadObject() :
-        test.defaultReadObject();
-    }
-}
-
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        InetSocketAddress crunchifyAddr = new InetSocketAddress("localhost", 4321);
-        SocketChannel crunchifyClient = SocketChannel.open(crunchifyAddr);
-
-        log("Connecting to Server on port 4321...");
-
-        Zalupa zalupa = new Zalupa(5);
-        System.out.println(zalupa.getSize());
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
-        oos.writeObject(zalupa);
-        byte [] data = bos.toByteArray();
-        crunchifyClient.write(ByteBuffer.wrap(data)); //write
-
-        crunchifyClient.close();
+    public static void main(String[] args) throws IOException {
+        ConsoleManager consoleManager = new ConsoleManager();
+        consoleManager.startInteractiveMode();
     }
-
-    private static void log(String str) {
-        System.out.println(str);
-    }
-
 }
