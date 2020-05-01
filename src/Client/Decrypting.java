dfe91a82c4e494fa435f66400312a84a02943c68
Vsevolod <vsevolod628@gmail.com>
2020-05-01 14:24:13 +0300

    вывод message от сервера

--- src/Client/Decrypting.java
+++ src/Client/Decrypting.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 
 public class Decrypting {
     static void decrypt(Object o) throws IOException {
-        if (o instanceof SerializedArgumentCommand) {
-            SerializedArgumentCommand serializedArgumentCommand = (SerializedArgumentCommand) o;
-            System.out.println(((SerializedArgumentCommand) o).getArg());
+        if (o instanceof SerializedMessage) {
+            SerializedMessage serializedMessage = (SerializedMessage) o;
+            System.out.println(((SerializedMessage) o).getMessage());
         }
     }
 }
