87289c7398820e5cffdb2969771c1261698f72f5
Vsevolod <vsevolod628@gmail.com>
2020-05-03 16:47:49 +0300

    ввод енамов не зависит от регистра

--- src/Commands/Utils/Readers/EnumReaders/CountryReader.java
+++ src/Commands/Utils/Readers/EnumReaders/CountryReader.java
@@ -15,12 +15,12 @@ public class CountryReader {
     public static Country read(String messageForConsole, boolean canBeNull) {
         Scanner in = new Scanner(System.in);
         System.out.print(messageForConsole + " Выберите страну из представленных(" + Arrays.asList(Country.values()) + "): ");
-        String toContains = in.nextLine().trim();
+        String toContains = in.nextLine().trim().toUpperCase();
 
         if ((!checkExist(toContains)) && !canBeNull && !toContains.equals("") || !canBeNull && toContains.equals("") || canBeNull && !toContains.equals("")) {
             while (!checkExist(toContains)) {
                 System.out.print("Вы ввели несуществующее значение из представленных. Попробуйте снова: ");
-                toContains = in.nextLine().trim();
+                toContains = in.nextLine().trim().toUpperCase();
                 checkExist(toContains);
                 if (canBeNull && toContains.equals("")) { return null; }
             }
--- src/Commands/Utils/Readers/EnumReaders/FormOfEducationReader.java
+++ src/Commands/Utils/Readers/EnumReaders/FormOfEducationReader.java
@@ -15,12 +15,12 @@ public class FormOfEducationReader {
     public static FormOfEducation read(boolean canBeNull) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите страну из представленных(" + Arrays.asList(FormOfEducation.values()) + "): ");
-        String toContains = in.nextLine().trim();
+        String toContains = in.nextLine().trim().toUpperCase();
 
         if ((!checkExist(toContains)) && !canBeNull && !toContains.equals("") || !canBeNull && toContains.equals("") || canBeNull && !toContains.equals("")) {
             while (!checkExist(toContains)) {
                 System.out.print("Вы ввели несуществующее значение из представленных. Попробуйте снова: ");
-                toContains = in.nextLine().trim();
+                toContains = in.nextLine().trim().toUpperCase();
                 checkExist(toContains);
                 if (canBeNull && toContains.equals("")) { return null; }
             }
